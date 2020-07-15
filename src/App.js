9dc14b7e439cb5091f4f2a51ebe8bc108a0254fc
Weider Lima <weider.lima@mercadolivre.com>
2020-07-14 21:24:59 -0300

    Change App from class to functional component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
-import React from 'react';
+import React, {useState} from 'react';
 
-class App extends React.Component {
-  render() {
-    return <h1>Hello world!</h1>;
-  }
-}
+const App = () => {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <h1>Hello world!</h1>
+      <h2>Count: {count}</h2>
+      <button onClick={() => setCount(count + 1)}>+</button>
+      <button onClick={() => setCount(count - 1)}>-</button>
+    </>
+  );
+};
 
 export default App;
