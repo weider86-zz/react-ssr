453e447751eccc8dec581e940007268eadf11f7a
Weider Lima <weider.lima@mercadolivre.com>
2020-07-14 23:31:13 -0300

    Add jest.config file and testing-library-react dependencies

---
+++ jest.config.js
@@ -0,0 +1,3 @@
+module.exports = {
+  setupTestFrameworkScriptFile: '<rootDir>/jestSetup.js',
+};
--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,10 @@
+import React from 'react';
+import {render} from '@testing-library/react';
+
+import App from './App';
+
 describe('<App />', () => {
-  it('should pass', () => {
-    expect(true).toBe(true);
-    expect(false).toBe(false);
+  it('should render', () => {
+    render(<App />);
   });
 });
