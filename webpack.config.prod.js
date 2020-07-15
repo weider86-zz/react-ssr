47dee246b63534c89e1fa44a021174b752ee226e
Weider Lima <weider.lima@mercadolivre.com>
2020-07-14 21:57:58 -0300

    Add bundle analyzer

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,6 +1,14 @@
+const {BundleAnalyzerPlugin} = require('webpack-bundle-analyzer');
 const baseConfig = require('./webpack.config.base');
 
 module.exports = {
   ...baseConfig,
   mode: 'production',
+  plugins: [
+    new BundleAnalyzerPlugin({
+      AnalyserNode: 'static',
+      openAnalyzer: false,
+      reportFilename: 'bundle_sizes.html',
+    }),
+  ],
 };
