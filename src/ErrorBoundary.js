46142a22b6bbe2d999c00d7323543aff6936919f
Weider Lima <weider.lima@mercadolivre.com>
2020-07-15 20:30:40 -0300

    Add prop-types

--- src/ErrorBoundary.js
+++ src/ErrorBoundary.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import propTypes from 'prop-types'
 
 export default class ErrorBoundary extends React.Component {
   state = {
@@ -7,6 +8,10 @@ export default class ErrorBoundary extends React.Component {
 
   static getDerivedStateFromError() {
     return { isError: true }
+  }
+
+  static propTypes = {
+    children: propTypes.node.isRequired,
   }
 
   render() {
