5d0e127e7ab1159bb11886a325fc6130d0eec8b6
Weider Lima <weider.lima@mercadolivre.com>
2020-07-14 21:43:40 -0300

    Add hot loader

--- webpack.config.base.js
+++ webpack.config.base.js
@@ -16,6 +16,7 @@ module.exports = {
         exclude: /node-modules/,
         options: {
           presets: ['@babel/preset-env', '@babel/preset-react'],
+          plugins: ['react-hot-loader/babel'],
         },
       },
       {
