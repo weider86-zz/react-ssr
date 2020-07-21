f682553d0bbebaacc6c184e728f9585d0fc9af7f
Weider Lima <weider.lima@mercadolivre.com>
2020-07-20 21:54:07 -0300

    Revert "Add react-axe to improve accessibility"
    This reverts commit 407abe1e76e980c24f551a04404def47dc4ab529.

--- src/index.js
+++ src/index.js
@@ -5,11 +5,6 @@ import { App } from './App'
 import ErrorBoundary from './ErrorBoundary'
 import './styles.css'
 
-if (process.env.NODE_ENV === 'development') {
-  const axe = require('react-axe')
-  axe(React, ReactDOM, 1000)
-}
-
 ReactDOM.render(
   <React.StrictMode>
     <ErrorBoundary>
