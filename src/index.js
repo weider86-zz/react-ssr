2e6dade4f029b3a4940fd63c0c20e4cf904e7729
Weider Lima <weider.lima@mercadolivre.com>
2020-08-04 19:51:05 -0300

    Change root element name

--- src/index.js
+++ src/index.js
@@ -5,11 +5,11 @@ import { App } from './App'
 import ErrorBoundary from './ErrorBoundary'
 import './styles.css'
 
-ReactDOM.render(
+ReactDOM.hydrate(
   <React.StrictMode>
     <ErrorBoundary>
       <App />
     </ErrorBoundary>
   </React.StrictMode>,
-  document.getElementById('app')
+  document.getElementById('root')
 )
