52d33ef688c56d3d823fb34a40d569e0373790f4
Weider Lima <weider.lima@mercadolivre.com>
2020-08-06 20:45:24 -0300

    Toggle render method

--- src/index.js
+++ src/index.js
@@ -1,15 +1,23 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+import { hydrate, render } from 'react-dom'
 import '@babel/polyfill'
 import { App } from './App'
 import ErrorBoundary from './ErrorBoundary'
-import './styles.css'
+import './assets/styles/main.css'
 
-ReactDOM.hydrate(
+const root = document.getElementById('root')
+var renderMethod
+if (root && root.innerHTML !== '') {
+  renderMethod = hydrate
+} else {
+  renderMethod = render
+}
+
+renderMethod(
   <React.StrictMode>
     <ErrorBoundary>
       <App />
     </ErrorBoundary>
   </React.StrictMode>,
-  document.getElementById('root')
+  root
 )
